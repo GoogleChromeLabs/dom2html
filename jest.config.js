02a44d7d21e61d52b8823cbee5554fc842c9a461
Dave Smart <dave@tamethebots>
2023-10-25 15:09:26 +0100

    fixes TextEncoder missing from JEST / jsdom tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   clearMocks: true,
   resetMocks: true,
   resetModules: true,
-  collectCoverage: true
+  collectCoverage: true,
+  testEnvironment: 'jsdom',
+  globals: {
+    TextEncoder: require('util').TextEncoder,
+    TextDecoder: require('util').TextDecoder,
+  },
 };
